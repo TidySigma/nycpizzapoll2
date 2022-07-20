38d48d980fe28ae8818cc58cd334d9a0cbc49b4d
Arham Choudhury <tidysigma@gmail.com>
2022-07-19 23:41:43 -0500

    update package list

--- init.R
+++ init.R
@@ -2,7 +2,7 @@
 #
 # Example R code to install packages if not already installed
 #
-my_packages = c("shiny", "plotly", "shinydashboard", "jsonlite")
+my_packages = c("shiny", "plotly", "shinydashboard", "jsonlite", "tidyverse")
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
